5f6ff7b3c92951eff23d9ddbeea36624b436f59c
akshay khenat <akshaykhenat0@gmail.com>
2025-12-15 23:32:41 +0530

    addes if else demo program

---
+++ basics/IfElseDemo.java
@@ -0,0 +1,10 @@
+class IfElseDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 5) {
+            System.out.println("a is greater than 5");
+        } else {
+            System.out.println("a is smaller");
+        }
+    }
+}
