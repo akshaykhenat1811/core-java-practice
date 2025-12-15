0c6e0609851617ad0d10666f947c69daa85bb3e2
akshay khenat <akshaykhenat0@gmail.com>
2025-12-15 23:07:51 +0530

    added VariablesDemo program

---
+++ basics/Variablesdemo.java
@@ -0,0 +1,8 @@
+class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 21;
+        String name = "Akshay";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
