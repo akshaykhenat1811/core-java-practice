a0924f86c2a62c0cf9927e375e7e44019667f360
akshay khenat <akshaykhenat0@gmail.com>
2025-12-22 19:36:29 +0530

    added core java practice file

---
+++ basics/switch_stattement.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class switch_stattement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int choice=sc.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;    
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("Jully");
+                break;  
+            case 8:
+                System.out.println("August");
+                break; 
+            case 9:
+                System.out.println("September");
+                break;  
+            case 10:
+                System.out.println("Octomber");
+                break;
+            case 11:
+                System.out.println("November");
+                break;     
+            case 12:
+                System.out.println("December");
+                break;              
+        }
+    }
+    
+}
